5d66ff560dcd7ebc01a1a252fa7695f11532621c
SandrinePatin <44065719+SandrinePatin@users.noreply.github.com>
2020-03-16 17:09:18 +0100

    Get /hello

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+app.get('/hello', function(req, res) {
+    if(req.query.nom === undefined){
+        res.send("Quel est votre nom?")
+    } else {
+        res.send("Bonjour, "+req.query.nom+"!")
+    }
+})
+
 app.listen(PORT, function () {
     console.log('Example app listening on port 3000!')
 })
