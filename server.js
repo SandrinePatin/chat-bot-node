cad017aded8b0357264ef494893905ef65c30021
SandrinePatin <44065719+SandrinePatin@users.noreply.github.com>
2020-03-17 14:32:23 +0100

    correction exercice 5

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const fs = require('fs')
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
@@ -19,10 +20,46 @@ app.get('/hello', function(req, res) {
 })
 
 app.post('/chat', function(req,res){
+
+    const entries = Object.values(req.body)
+
+    
+    /*if(json !== undefined){
+    	const jsonVariable = JSON.parse(json)
+	console.log(jsonVariable)
+    }
+    const newEntry = entries[0].split("=")
+
+    const key = newEntry[0]
+    console.log(key)
+    const value = newEntry[1]
+
+    jsonString = "{"+ key +":"+ value +"}"
+    
+    fs.writeFileSync("reponses.json", jsonString, function(err) {
+         if(err) {
+             console.log(err);
+         } else {
+             res.send("Merci pour cette information !");
+         }
+    })*/
+
     if(req.body.msg === 'meteo'){
         res.send("Il fait beau")
     } else if(req.body.msg === 'ville'){
         res.send("Nous sommes a Paris")
+    } else {
+	if(/ = /.test(req.body.msg)){
+	    const [ cle, valeur ] = req.body.msg.split(" = ")
+	    fs.writeFileSync('reponses.json', JSON.stringify({ [cle]: valeur}))
+	} else {
+        const cle = req.body.msg
+        const reponses = fs.readFileSync('réponses.json', { encoding: 'utf8' })
+        const reponse = JSON.parse(reponses)[cle]
+        res.send(cle + ': ' + reponse)
+	    
+	}
+	res.send(req.body.msg)
     }
 })
 
